75f74e4b8cffb496ce5796b4b638bce47ff8f405
ujjwaldhiman003@gmail.com <ujjwaldhiman003@gmail.com>
2023-09-11 05:28:22 +0530

    corrections

--- resources/views/home/userDetails.blade.php
+++ resources/views/home/userDetails.blade.php
@@ -10,23 +10,23 @@
                 <div class="card-body">
                     <form action="{{ route('user-details.store') }}" method="POST">
                         @csrf
-                        <div class="form-group">
+                        <div class="form-group form-floating mb-3">
                             <label for="first_name">First Name</label>
                             <input type="text" name="first_name" id="first_name" class="form-control" required>
                         </div>
-                        <div class="form-group">
+                        <div class="form-group form-floating mb-3">
                             <label for="last_name">Last Name</label>
                             <input type="text" name="last_name" id="last_name" class="form-control" required>
                         </div>
-                        <div class="form-group">
+                        <div class="form-group form-floating mb-3">
                             <label for="city">City</label>
                             <input type="text" name="city" id="city" class="form-control" required>
                         </div>
-                        <div class="form-group">
+                        <div class="form-group form-floating mb-3">
                             <label for="state">State</label>
                             <input type="text" name="state" id="state" class="form-control" required>
                         </div>
-                        <div class="form-group">
+                        <div class="form-group form-floating mb-3">
                             <label for="address">Address</label>
                             <textarea name="address" id="address" class="form-control" required></textarea>
                         </div>
